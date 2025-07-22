b8c2f3ec4604ce95c31a370eca3dcf901f0671ca
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-22 16:13:06 +0530

    Update Smallest Positive Missing.c

--- Smallest Positive Missing.c
+++ Smallest Positive Missing.c
@@ -13,6 +13,7 @@ class Solution {
         for(int i =0 ;i<n;i++){
             if(arr[i]!=i+1){
                 return i+1;
-            
-    }
+            }
+        }
+        return n+1;    
 };
