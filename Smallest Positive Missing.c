1b80a44efba6459c9b7951ecfbd0fa296b471000
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-22 16:10:53 +0530

    Update Smallest Positive Missing.c

--- Smallest Positive Missing.c
+++ Smallest Positive Missing.c
@@ -2,6 +2,11 @@ class Solution {
   public:
     int missingNumber(vector<int> &arr) {
         // code here
+        int n = arr.size();
         
+        for(int i = 0;i<n;i++){
+            while(arr[i]>0 && arr[i]<=n  && arr[arr[i] - 1] != arr[i]){
+                swap(arr[i],arr[arr[i]-1]);
+            
     }
 };
