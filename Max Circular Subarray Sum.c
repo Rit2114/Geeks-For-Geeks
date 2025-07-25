1278cf98704a5747b13eaa01222529fba7869dad
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-25 23:00:46 +0530

    Update Max Circular Subarray Sum.c

--- Max Circular Subarray Sum.c
+++ Max Circular Subarray Sum.c
@@ -5,5 +5,7 @@ class Solution {
     int curr_max=arr[0], max_sum=arr[0];
     int curr_min=arr[0], min_sum=arr[0];
     int total= arr[0];   
+    for(int i=1;i<arr.size();i++){
+            curr_max=max(arr[i],curr_max+arr[i]);
     }
 };
