29acc2b699e23f98d21416458530777ca8303f91
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-25 22:59:58 +0530

    Update Max Circular Subarray Sum.c

--- Max Circular Subarray Sum.c
+++ Max Circular Subarray Sum.c
@@ -1,7 +1,9 @@
 class Solution {
   public:
     int maxCircularSum(vector<int> &arr) {
-     // code here
-        
+    // code here
+    int curr_max=arr[0], max_sum=arr[0];
+    int curr_min=arr[0], min_sum=arr[0];
+    int total= arr[0];   
     }
 };
