4aeb5470615aed29c2e91acae0f6b4d52db0dcb8
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-26 17:32:09 +0530

    Update .c

--- .c
+++ .c
@@ -1,6 +1,11 @@
 class Solution {
   public:
     vector<int> findMajority(vector<int>& arr) {
-        
+        map<int,int>mp;
+       vector<int>ans;
+       int n=arr.size()/3;
+       for(int i=0;i<arr.size();i++){
+           mp[arr[i]]++;
+       }
     }
 };
