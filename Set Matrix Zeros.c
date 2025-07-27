1d516a09d90d7ffdf1df8703695c5ab083aac1ea
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-27 22:25:43 +0530

    Update Set Matrix Zeros.c

--- Set Matrix Zeros.c
+++ Set Matrix Zeros.c
@@ -1,5 +1,10 @@
 class Solution {
   public:
     void setMatrixZeroes(vector<vector<int>> &mat) {
+      int n=mat.size();
+        int m=mat[0].size();
+        
+        vector<int>rows(n, -1);
+        vector<int>cols(m, -1);
     }
 };
