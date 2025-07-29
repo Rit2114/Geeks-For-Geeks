7bee238ca1c502bc44b7af233fc8557fd1bc0c62
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-29 19:39:02 +0530

    Update .c

--- .c
+++ .c
@@ -2,6 +2,10 @@ class Solution {
   public:
     vector<int> asciirange(string& s) {
         // code here
-        
+        int n = s.size();
+        vector<int>ans;
+        vector<int>freq(26, -1);
+        vector<int>prefixsum(26, 0);
+        int sum = 0;
     }
 };
