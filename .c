ed4012eac58e7430bdda4d5f903d8efa1e93a742
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-08 03:32:20 +0530

    Update .c

--- .c
+++ .c
@@ -2,6 +2,8 @@ class Solution {
   public:
     int getLPSLength(string &s) {
         // code here
-        
+        int n=s.size();
+        vector<int> ans(n,0);
+        int prefix=0,suffix=1;
     }
 };
