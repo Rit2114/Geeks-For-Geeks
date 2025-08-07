d7311b5195b630e2450840fe62f87def9e8c2a94
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-08 03:32:50 +0530

    Update .c

--- .c
+++ .c
@@ -5,5 +5,11 @@ class Solution {
         int n=s.size();
         vector<int> ans(n,0);
         int prefix=0,suffix=1;
+        while(suffix<n){
+            if(s[prefix]==s[suffix]){
+                ans[suffix]=prefix+1;
+                prefix++;
+                suffix++;
+            }
     }
 };
