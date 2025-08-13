618be12bf2c7be5a30e59741d5dbd7939eee7725
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-14 00:04:43 +0530

    Update Tywin's War Strategy.c

--- Tywin's War Strategy.c
+++ Tywin's War Strategy.c
@@ -2,6 +2,8 @@ class Solution {
   public:
     int minSoldiers(vector<int>& arr, int k) {
         // code here
-      
+      int n=arr.size();
+        int safe_troop=(n+1)/2;
+        for(int i=0;i<n;i++)
     }
 };
