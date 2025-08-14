825e51a555a26541a51ed2c424d2de3ea8600516
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-14 19:11:22 +0530

    Update .py

--- .py
+++ .py
@@ -1,4 +1,16 @@
 class Solution:
     def countRevPairs(self, arr):
         # Code here
-        
+        def merge_sort(nums, left, right):
+            if left >= right:
+                return 0
+            mid = (left + right) // 2
+            count = merge_sort(nums, left, mid) + merge_sort(nums, mid + 1, right)
+            
+            # Count cross reverse pairs
+            j = mid + 1
+            for i in range(left, mid + 1):
+                while j <= right and nums[i] > 2 * nums[j]:
+                    j += 1
+                count += (j - (mid + 1))
+                
