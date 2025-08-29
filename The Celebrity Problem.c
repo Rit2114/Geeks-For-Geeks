2476ca8d566a31ad9186efaa566019c653c392ad
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-30 01:25:19 +0530

    Update The Celebrity Problem.c

--- The Celebrity Problem.c
+++ The Celebrity Problem.c
@@ -2,6 +2,9 @@ class Solution {
   public:
     int celebrity(vector<vector<int>>& mat) {
         // code here
-        
+        int n = mat.size();
+                int m = mat[0].size();
+                      
+                for(int i=0;i<n;i++){
     }
 };
