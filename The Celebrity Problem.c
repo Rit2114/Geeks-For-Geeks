1d900f39fa7690446ca720226390450ca5a2dfb1
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-30 01:28:07 +0530

    Update The Celebrity Problem.c

--- The Celebrity Problem.c
+++ The Celebrity Problem.c
@@ -1,15 +1,19 @@
 class Solution {
   public:
-    int celebrity(vector<vector<int>>& mat) {
+    int celebrity(vector<vector<int>>& mat) 
+    {
         // code here
         int n = mat.size();
                 int m = mat[0].size();
                       
-                for(int i=0;i<n;i++){
+                for(int i=0;i<n;i++)
+                {
                    bool flag = true;
-                   for(int j=0;j<n;j++){                              
+                   for(int j=0;j<n;j++)
+                     {                              
                                   if (i==j) continue;
-                                  if ( (mat[i][j]==1 ||  mat[j][i]==0) ){
+                                  if ( (mat[i][j]==1 ||  mat[j][i]==0) )
+                                  {
                                         flag =false;
                                         break;
                                   }                              
