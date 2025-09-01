46998e73701e07991c787198273fe8e406a70a86
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-01 21:17:00 +0530

    Update Sum of Mode.c

--- Sum of Mode.c
+++ Sum of Mode.c
@@ -2,6 +2,16 @@ class Solution {
   public:
     int sumOfModes(vector<int>& arr, int k) {
         // code here
+        map<int,int>mp;
         
+        int n = arr.size();
+        for(int i = 0; i < k; i++){
+            mp[arr[i]]++;
+        }
+        multiset<int>am;
+        vector<int>freq;
+        for(auto i : mp){
+            am.insert(i.second);
+        }
     }
 };
