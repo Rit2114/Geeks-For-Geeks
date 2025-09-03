2a5f75eb09c1ba893af1dcb5a4cc0846ed6feb96
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-03 23:36:17 +0530

    Update Reverse a Doubly Linked List.c

--- Reverse a Doubly Linked List.c
+++ Reverse a Doubly Linked List.c
@@ -2,6 +2,9 @@ class Solution {
   public:
     Node *reverse(Node *head) {
         // code here
-        
+        Node* temp1=head;
+        while(temp1){
+            Node *prev=temp1->prev;
+            Node *next=temp1->next;
     }
 };
