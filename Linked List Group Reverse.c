3772448137266d045db0271209d33075942541b4
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-04 22:12:32 +0530

    Update Linked List Group Reverse.c

--- Linked List Group Reverse.c
+++ Linked List Group Reverse.c
@@ -2,6 +2,8 @@ class Solution {
   public:
     Node *reverseKGroup(Node *head, int k) {
         // code here
-        
+        if (!head || k==1) return head;
+        Node* temp = head, *prv = NULL, *nxt = NULL;
+        for(int i=0;i<k && temp;i++)
     }
 };
