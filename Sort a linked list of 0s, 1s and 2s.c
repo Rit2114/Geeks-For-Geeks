68d9972eddcd3da6de3af749977391e9226b2a1a
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-05 17:40:34 +0530

    Update Sort a linked list of 0s, 1s and 2s.c

--- Sort a linked list of 0s, 1s and 2s.c
+++ Sort a linked list of 0s, 1s and 2s.c
@@ -2,6 +2,26 @@ class Solution {
   public:
     Node* segregate(Node* head) {
         // code here
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        Node* temp = head;
+        while(temp!=NULL)
+        {
+            if(temp->data==0)
+            {
+                count0++;
+            }
+            if(temp->data==1)
+            {
+                count1++;
+            }
+            if(temp->data==2)
+            {
+                count2++;
+            }
+            temp=temp->next;
+        }
         
     }
 };
