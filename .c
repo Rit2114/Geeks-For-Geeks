5d5332073aea0f1b3cbf3d9700aa43845f4a14b2
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-11 22:33:49 +0530

    Update .c

--- .c
+++ .c
@@ -2,6 +2,13 @@ class Solution {
   public:
     int minJumps(vector<int>& arr) {
         // code here
+        int n = arr.size();
+        if (n <= 1) return 0;        
+        if (arr[0] == 0) return -1;   
+
+        int maxReach = arr[0];  
+        int steps = arr[0];    
+        int jumps = 1;          
         
     }
 };
