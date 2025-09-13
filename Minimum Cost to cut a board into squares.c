53c9e450dc43e3a2e3fe6e93405c575bcec92b8a
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-13 18:43:13 +0530

    Update Minimum Cost to cut a board into squares.c

--- Minimum Cost to cut a board into squares.c
+++ Minimum Cost to cut a board into squares.c
@@ -6,6 +6,13 @@ class Solution {
         for(auto i:x) pq.push({i , 0});
         for(auto i:y) pq.push({i , 1});
         int hor_cut = 0,ver_cut = 0,cost = 0;
+        while(!pq.empty()){
+            auto p = pq.top();
+            pq.pop();
+            if(p.second == 0){
+                hor_cut++;
+                cost += p.first * (ver_cut+1);
+            }
     }
 };
 
