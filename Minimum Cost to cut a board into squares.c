5bd304a38daa7d888a4568ea432fe149237eccac
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-13 18:42:42 +0530

    Update Minimum Cost to cut a board into squares.c

--- Minimum Cost to cut a board into squares.c
+++ Minimum Cost to cut a board into squares.c
@@ -2,7 +2,10 @@ class Solution {
   public:
     int minCost(int n, int m, vector<int>& x, vector<int>& y) {
         //  code here
-        
+        priority_queue<pair<int,int>> pq;
+        for(auto i:x) pq.push({i , 0});
+        for(auto i:y) pq.push({i , 1});
+        int hor_cut = 0,ver_cut = 0,cost = 0;
     }
 };
 
