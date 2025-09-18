e6866d60a046b5afe927b41f73c9a1cd60f325b0
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-18 23:48:06 +0530

    Update Next Greater Element in Circular Array.c

--- Next Greater Element in Circular Array.c
+++ Next Greater Element in Circular Array.c
@@ -1,7 +1,15 @@
 class Solution {
+  public:class Solution {
   public:
     vector<int> nextGreater(vector<int> &arr) {
         // code here
+        int n=arr.size();
+        stack<int> st;
+
+        vector<int> v(n,-1);
+        int i=2*n-1;
+        
         
     }
 };
+    
