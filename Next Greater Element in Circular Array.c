d05687ffc9996b42581a37fcf418a10797e9c725
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-18 23:48:32 +0530

    Update Next Greater Element in Circular Array.c

--- Next Greater Element in Circular Array.c
+++ Next Greater Element in Circular Array.c
@@ -9,6 +9,18 @@ class Solution {
         vector<int> v(n,-1);
         int i=2*n-1;
         
+        while(i>=0){
+            int idx= i>=n?i-n:i;
+            
+            while(st.size() and st.top()<=arr[idx]) st.pop();
+            
+            if(!st.empty() && i<n) v[i]=st.top();
+            
+            st.push(arr[idx]);
+
+            i--;
+        }
+        
         
     }
 };
