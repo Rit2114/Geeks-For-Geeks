5781fad7c71441be7e1a87862310fccf2bb584cf
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-10-17 12:45:05 +0530

    Update BST to greater sum tree.c

--- BST to greater sum tree.c
+++ BST to greater sum tree.c
@@ -2,6 +2,11 @@ class Solution {
   public:
     void transformTree(Node *root) {
         // code here
-        
+        if(!root){return;}
+      solve(root->right,sum);
+      int rootval=root->data;
+      root->data=sum;
+      sum+=rootval;
+      solve(root->left,sum);
     }
 };
